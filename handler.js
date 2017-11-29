3130ce45650f7b635d496ddecda95ddf8fcc2971
Jon Stokes <jon.m.stokes@gmail.com>
2017-11-29 10:52:28 -0600

    First stab at trigger

--- handler.js
+++ handler.js
@@ -4,7 +4,6 @@ module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     headers: {
-      "X-Powered-By": "foobarbaz",
       "Content-Type": "application/json",      
       "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
       "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
@@ -21,3 +20,10 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.postSignup = (event, context, callback) => {
+  console.log(JSON.stringify(event.request.userAttributes))
+  console.log(JSON.stringify(context))
+  // Return result to Cognito
+  context.done(null, event);      
+}
