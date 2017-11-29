66734054bf5afffc1bf68398a890505d054c02d6
Jon Stokes <jon.m.stokes@gmail.com>
2017-11-28 18:40:58 -0600

    Retry the headers

--- handler.js
+++ handler.js
@@ -5,8 +5,9 @@ module.exports.hello = (event, context, callback) => {
     statusCode: 200,
     headers: {
       "X-Powered-By": "foobarbaz",
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin" : "*" // Required for CORS support to work
+      "Content-Type": "application/json",      
+      "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
     },
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
